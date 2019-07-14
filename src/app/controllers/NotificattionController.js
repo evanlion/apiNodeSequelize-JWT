9a1ef1a94604f9bfdc3ad99145b8b968d8e97f8d
Dev <evangelistalion@gmail.com>
2019-07-14 01:39:20 -0300

    uploads avatar and notifications with mongo

---
+++ src/app/controllers/NotificattionController.js
@@ -0,0 +1,36 @@
+import Notificattion from '../schemas/Notification';
+import User from '../models/User';
+
+class NotificattionController {
+  async index(req, res) {
+    const isProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Only provider can load notifications' });
+    }
+
+    const notifications = await Notificattion.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return res.json(notifications);
+  }
+
+  async update(req, res) {
+    const notification = await Notificattion.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+
+    return res.json(notification);
+  }
+}
+
+export default new NotificattionController();
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificattionController from './app/controllers/NotificattionController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -12,5 +20,17 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificattionController.index);
+routes.put('/notifications/:id', NotificattionController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
