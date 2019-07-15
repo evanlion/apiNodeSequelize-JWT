b97565b851e8deb820b4faca55e2db064dc42cc2
Dev <evangelistalion@gmail.com>
2019-07-14 21:05:26 -0300

    email template hbs

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'be6e665f8b40e7',
+    pass: 'a49718b4c866a0',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
